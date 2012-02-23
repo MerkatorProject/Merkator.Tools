adb2ba353b705044e5453e08ae9a9352ff96fb82
CodesInChaos <CodeInChaos@quantentunnel.de>
2012-02-23 13:52:03 +0100

    Added Test Project

--- Merkator.Tools.Tests/Properties/AssemblyInfo.cs
+++ Merkator.Tools.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Merkator.Tools")]
+[assembly: AssemblyTitle("Merkator.Tools.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Merkator.Tools")]
+[assembly: AssemblyProduct("Merkator.Tools.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d31b8384-aa13-4a3b-934d-757afa221e42")]
+[assembly: Guid("2ccddb10-8f99-4795-9bae-c5ac323e141b")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,8 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: InternalsVisibleTo("Merkator.Tools.Tests")]
